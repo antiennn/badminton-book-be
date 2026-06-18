28801f9e6c884572019826973b54436b9f29b9c5
Ralph Le <ralph.le@ascendasiacs.com>
2026-06-18 11:30:21 +0700

    chore: update cors for production

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin:
-      'http://localhost:5000',
-
+      ['http://localhost:5000', 'https://badminton-book-fe.vercel.app/'],
     credentials: true,
   });
 
